37afbac73215a22853eab884ffcfe111e8d17519
ddururiiiiiii <78954385+ddururiiiiiii@users.noreply.github.com>
2021-10-20 19:46:11 +0900

    백준 2439 - for문 : 별 찍기 - 2

--- Baekjoon/src/level3/level3_2439.java
+++ Baekjoon/src/level3/level3_2439.java
@@ -10,11 +10,14 @@ public class level3_2439 {
 		
 		int num = scan.nextInt();
 
-		for (int i = num; i >= 0; i--) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print("☆");
+		for (int i = 1; i <= num; i++) {
+			for (int j = 1; j <= num - i; j++) {
+				System.out.print(" ");
 			}
-			System.out.println("★");
+			for (int k = 1; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
 		}
 	}
 	
