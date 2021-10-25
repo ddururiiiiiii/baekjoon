da2a1e521549dad544476c7130a0397eff057421
ddururiiiiiii <78954385+ddururiiiiiii@users.noreply.github.com>
2021-10-25 20:45:35 +0900

    baekjoon - 2741

---
+++ Baekjoon/src/level3/level3_2741.java
@@ -0,0 +1,18 @@
+package level3;
+
+import java.util.Scanner;
+
+public class level3_2741 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+				
+		int num = scanner.nextInt();
+		
+		for(int i = 1; i<= num; i++) {
+			System.out.println(i);
+		}
+		
+	}
+}
