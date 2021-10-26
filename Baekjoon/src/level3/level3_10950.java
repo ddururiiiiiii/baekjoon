6a2de12e06d8bfcc5f4b7cdc90b7b0b41b5cbbda
ddururiiiiiii <78954385+ddururiiiiiii@users.noreply.github.com>
2021-10-26 20:33:29 +0900

    beakjoon - 10950

---
+++ Baekjoon/src/level3/level3_10950.java
@@ -0,0 +1,21 @@
+package level3;
+
+import java.util.Scanner;
+
+public class level3_10950 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int num = scan.nextInt();
+		
+		for (int i = 0; i < num; i++) {
+			int num1 = scan.nextInt();
+			int num2 = scan.nextInt();
+			System.out.println(num1 + num2);
+		}
+
+	}
+	
+}
