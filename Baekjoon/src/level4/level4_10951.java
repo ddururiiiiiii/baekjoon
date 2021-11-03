7431cda3d2b4f193a4ec8fce9885796c5812d27d
ddururiiiiiii <78954385+ddururiiiiiii@users.noreply.github.com>
2021-11-04 05:46:31 +0900

    baekjoon 10951

--- Baekjoon/src/level4/level4_10951.java
+++ Baekjoon/src/level4/level4_10951.java
@@ -2,25 +2,16 @@ package level4;
 
 import java.util.Scanner;
 
-public class level4_10952 {
-	
-	
+public class level4_10951 {
+
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
-		
-		
-		while(true) {
+
+		while (scan.hasNextInt()) {
 			int a = scan.nextInt();
 			int b = scan.nextInt();
-
-			if (a == 0 && b == 10) {
-				break;
-			}
-			System.out.println(a+b);
+			System.out.println(a + b);
 		}
-		
-		
-		
 	}
 }
