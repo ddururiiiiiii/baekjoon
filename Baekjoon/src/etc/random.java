ab00b61a787d4de2b6ce380dca5c185358fe7c6f
ddururiiiiiii <78954385+ddururiiiiiii@users.noreply.github.com>
2021-11-11 19:54:53 +0900

    etc. 랜덤 난수 만들기 2가지 방법.

---
+++ Baekjoon/src/etc/random.java
@@ -0,0 +1,28 @@
+package etc;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class random {
+	
+	
+	public static void main(String[] args) {
+			
+		Scanner scan = new Scanner(System.in);
+		
+		int num = scan.nextInt();
+		
+		// 랜덤숫자 만들기 - random 객체, setseed 함수 이용
+		// 범위  : 1 ~ 100
+		Random randomNum1 = new Random();
+		randomNum1.setSeed(System.currentTimeMillis());
+		System.out.print(randomNum1.nextInt(100) + 1);
+		
+		// 랜덤숫자 만들기 - Math.random() 함수 이용
+		// 범위 : 1 ~ 100
+		int randomNum2 = (int)(Math.random() * 99 ) + 1;
+		System.out.println(randomNum2);
+		
+		
+	}
+}
